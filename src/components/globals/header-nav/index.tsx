52808585f54e114513224dffb79b1e9aa76591e8
rakeshkumarsah2311 <rakeshkumarsah2311@gmail.com>
2025-05-18 20:55:08 +0530

    mail to

--- src/components/globals/header-nav/index.tsx
+++ src/components/globals/header-nav/index.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link"
-import { NavItem } from "./nav-item"
-import { MobileNavItem } from "./mobile-nav-item"
-import { MobileNavbar } from "./mobile-navbar"
+import Link from "next/link";
+import { NavItem } from "./nav-item";
+import { MobileNavItem } from "./mobile-nav-item";
+import { MobileNavbar } from "./mobile-navbar";
 import Image from "next/image";
-import NavLogo from "@/assets/brand-logos/nav-logo.png"
+import NavLogo from "@/assets/brand-logos/nav-logo.png";
 
 const Navbar = () => {
   return (
-    <header className=" sticky top-0 z-50 bg-white shadow-lg">
+    <header className="sticky top-0 z-50 bg-white shadow-lg">
       <div className="container flex items-center justify-between gap-10 py-2">
         <Link href="/" className="flex items-center gap-3 w-38 h-20 relative">
           <Image src={NavLogo} alt="Akriti Sky Morph logo" fill className="object-contain" />
@@ -16,8 +16,8 @@ const Navbar = () => {
           <nav className="hidden items-center gap-10 md:flex justify-end uppercase">
             <NavItem label="Services" href="#services" />
             <NavItem label="Solutions" href="#solutions" />
-            <NavItem label="About Akriti Sky Morph " href="#about" />
-            <NavItem label="Say Hey" href="sales@skymorphs.com" />
+            <NavItem label="About Akriti Sky Morph" href="#about" />
+            <NavItem label="Say Hey" href="mailto:sales@skymorphs.com" />
           </nav>
         </div>
         <MobileNavbar>
@@ -26,13 +26,13 @@ const Navbar = () => {
               <MobileNavItem label="Services" href="#services" />
               <MobileNavItem label="Solutions" href="#solutions" />
               <MobileNavItem label="About Akriti Sky Morph" href="#about" />
-              <MobileNavItem label="Say Hey" href="/say-hey" />
+              <MobileNavItem label="Say Hey" href="mailto:sales@skymorphs.com" />
             </nav>
           </div>
         </MobileNavbar>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
